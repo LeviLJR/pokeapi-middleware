071213f8e243f4ab6578ca776ed29b9a88191375
LeviLJR <levilibermanjunior@gmail.com>
2024-12-13 02:10:42 -0300

    feat(pokemon): add middleware to fetch and return Pokémon abilities

---
+++ server.js
@@ -0,0 +1,55 @@
+// Import modules
+const express = require("express");
+const axios = require("axios");
+//Enabling requests and responses
+const app = express();
+const PORT = 3000;
+
+// Get a list of Pokémon's names
+app.get("/api/pokemons", async (req, res) => {
+  try {
+    // Fetch Pokémon names from the first page
+    const abilitiesResponse = await axios.get(
+      "https://pokeapi.co/api/v2/pokemon/"
+    );
+    const pokemonList = abilitiesResponse.data.results.map(
+      (pokemon) => pokemon.name
+    );
+
+    // Display a few examples
+    res.json({
+      message: "Here are some example Pokémon names you can use",
+      examples: pokemonList, // Limita a 10 exemplos
+    });
+  } catch (error) {
+    res.status(500).send("Error fetching Pokémon names.");
+  }
+});
+
+// Route to fetch Pokémon abilities
+app.get("/api/pokemons/:poke_name", async (req, res) => {
+  const { poke_name } = req.params;
+
+  try {
+    // Fetch Pokémon data
+    const abilitiesResponse = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${poke_name}`
+    );
+
+    // Extract and sort abilities
+    const abilities = abilitiesResponse.data.abilities
+      .map((ability) => ability.ability.name)
+      .sort();
+    res.json({
+      name: poke_name,
+      abilities: abilities,
+    });
+  } catch (error) {
+    res.status(500).send("Pokemon ${poke_name} not found");
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/api/pokemons/`);
+});
